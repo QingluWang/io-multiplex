571be7fe2fe8114d1fa5364d9784229e369dfa4a
wql <18353184289@163.com>
2019-01-11 19:40:01 +0800

    1

--- server/my_server.c
+++ server/my_server.c
@@ -15,9 +15,8 @@ int ServerInit(uint16_t port){
     int sockId=TcpCreate();
     int set = 1;  
     setsockopt(sockId, SOL_SOCKET, SO_REUSEADDR, &set, sizeof(int)); 
-    struct sockaddr_in server,client;
+    struct sockaddr_in server;
     memset(&server,0,sizeof(server));
-    memset(&client,0,sizeof(client));
     server.sin_family = AF_INET;
     server.sin_port = htons(port);
     server.sin_addr.s_addr = INADDR_ANY;
@@ -31,6 +30,25 @@ int ServerInit(uint16_t port){
     };
     return sockId;
 }
-void ServerListen(int sockId){
+void ServerListen(int serverSockId){
+    while(1){
+        struct sockaddr_in client;
+        socklen_t   length = sizeof(client_addr);
+        int clientSockId = accept(serverSockId, (struct sockaddr*)&client, &length);
+        if (clientSockId < 0)
+        {
+            printf("ServerListen:server accept failed!\n");
+            break;
+        }
 
+        char buffer[MAX_LEN];
+        bzero(buffer, sizeof(buffer));
+        length = recv(clientSockId, buffer, MAX_LEN, 0);
+        if (length < 0)
+        {
+            printf("ServerListen:server recieve data failed!\n");
+            break;
+        }
+        printf("Received:%s\n",buffer);
+    }
 }
--- server/server_main.c
+++ server/server_main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include "my_server.h"
 
+static const uint16_t port=17989;
 int main(int argc,char* argv[]){
-
+    int serverSockId=ServerInit();
+    serverListen(port);
     return 0;
 }
