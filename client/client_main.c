812e190e23f5973680530a359dea981e55d19981
wql <18353184289@163.com>
2019-01-12 10:24:22 +0800

    1

--- client/client_main.c
+++ client/client_main.c
@@ -3,12 +3,16 @@
 #include <stdint.h>
 #include <unistd.h>
 #include "my_client.h"
+#include "../util/my_util.h"
 
 static const char serverAddr[]="127.0.0.1";
 static const uint16_t port=17989;
 int main(int argc,char* argv[]){
     int sockId=ClientConnect(serverAddr,port);
-    ClientSendMsg(sockId,"1");
+    char buffer[MAX_LEN];
+    bzero(buffer,sizeof(buffer));
+    int num=RandomNum(1000);
+    ClientSendMsg(sockId,itoa(num,buffer,10));
     close(sockId);
     return 0;
 }
--- util/my_util.h
+++ util/my_util.h
@@ -1,7 +1,8 @@
 #ifndef _MY_UTIL_H
 #define _MY_UTIL_H
 
-#define SERVER_ADDRESS "127.0.0.1"
 #define MAX_LEN 128
+
+int RandomNum(int max);
 
 #endif
