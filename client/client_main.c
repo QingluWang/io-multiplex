eb24048866b6dfaa773f0cabdf8eb4093a98af03
wql <18353184289@163.com>
2019-01-13 10:47:07 +0800

    1

--- client/client_main.c
+++ client/client_main.c
@@ -3,21 +3,33 @@
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include "my_client.h"
 #include "../util/my_util.h"
 
 static const char serverAddr[]="127.0.0.1";
 static const uint16_t port=17989;
+static const int childProcessCount=1000;
 int main(int argc,char* argv[]){
-    char buffer[MAX_LEN];  
-    bzero(buffer,sizeof(buffer));
-    for(int i=0; i<MAXEPOLLSIZE; i++){
+    pid_t pid;
+    int n=0;
+    for(; n<childProcessCount; n++){
+        pid=fork();
+        if(0 == pid || -1 == pid)
+            break;
+    }
+    if(-1 == pid)
+        printf("NO.%d fork error!\n",n);
+    else if(0 == pid){
+        char buffer[MAX_LEN];  
+        bzero(buffer,sizeof(buffer));
         int sockId=ClientConnect(serverAddr,port);
-        //int num=RandomNum(1000);
-        sprintf(buffer,"%d",i);
+        int sendNum=RandomNum(childProcessCount);
+        sprintf(buffer,"%d",sendNum);
         ClientSendMsg(sockId,buffer);
         close(sockId);
         printf("Send:%s\n",buffer);
+        return 0;
     }
     return 0;
 }
