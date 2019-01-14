798be7a845779b8fdf31e410cb594d4f9e8d82e1
wql <18353184289@163.com>
2019-01-14 13:32:52 +0800

    1

--- client/client_main.c
+++ client/client_main.c
@@ -4,15 +4,36 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <time.h>
 #include "my_client.h"
 #include "../util/my_util.h"
+
 
 static const char serverAddr[]="127.0.0.1";
 static const uint16_t port=17989;
 
 int main(int argc,char* argv[]){
-    pid_t pid;
     int sockFd=ClientConnect(serverAddr,port);
-    ClientSendMsg(sockFd,argv[1]);
+    pid_t pid=fork();
+    if(pid < 0){
+        perror("client_main:create child thread error!");
+    }
+    else if(pid == 0){
+        int receBytes=0;
+        char buffer[MAX_LEN];
+        memset(buffer,0,sizeof(buffer));
+        while((receBytes=recv(sockFd,buffer,MAX_LEN,0)) > 0){
+            printf("Receive:%s\n",buffer);
+            receBytes=0;
+        }
+    }
+    else{
+        ClientSendMsg(sockFd,argv[1]);
+    }
     return 0;
 }
